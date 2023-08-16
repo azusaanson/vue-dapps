335914b061b57cda1fbeed747413ab4b1edd6da7
azusaanson <yth041512@gmail.com>
2023-08-17 01:16:09 +0900

    renew local MYGOVERNOR_ADDRESS

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -3,4 +3,4 @@ export const MYTOKEN_SYMBOL = "MTK";
 
 // addresses for local hardhat node, please change if using main or test-net
 export const MYTOKEN_ADDRESS = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
-export const MYGOVERNOR_ADDRESS = "0x9fe46736679d2d9a65f0992f2272de9f3c7fa6e0";
+export const MYGOVERNOR_ADDRESS = "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512";
